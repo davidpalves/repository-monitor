1e2a0974b9d039a450dd616f54909271be6ecdb0
David Pierre <davidpierrealves21@gmail.com>
2019-11-24 13:17:20 -0300

    Add model tests to users

--- backend/users/tests/test_models.py
+++ backend/users/tests/test_models.py
@@ -6,42 +6,42 @@ from model_mommy import mommy
 class TestUser(TestCase):
 
     def setUp(self):
-        self.instance = mommy.make('users.User', username='test_username')
+        self.user = mommy.make('users.User', username='test_username')
 
     def test_get_email(self):
         self.assertEqual(
-            self.instance.get_email(),
-            self.instance.email
+            self.user.get_email(),
+            self.user.email
         )
 
     def test_get_short_name(self):
         self.assertEqual(
-            self.instance.get_username(),
-            self.instance.username
+            self.user.get_username(),
+            self.user.username
         )
 
     def test_str(self):
         self.assertEqual(
-            str(self.instance),
-            self.instance.email
+            str(self.user),
+            self.user.email
         )
 
     def tearDown(self):
-        self.instance.delete()
+        self.user.delete()
 
 
 class TestGithubUser(TestCase):
 
     def setUp(self):
         self.user = mommy.make('users.User', username='test_username')
-        self.instance = mommy.make('users.GithubUser', user=self.user)
+        self.github_user = mommy.make('users.GithubUser', user=self.user)
 
     def test_str(self):
         self.assertEqual(
-            str(self.instance),
+            str(self.github_user),
             self.user.username
         )
 
     def tearDown(self):
-        self.instance.delete()
+        self.github_user.delete()
         self.user.delete()
