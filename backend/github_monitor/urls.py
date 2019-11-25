f5dc233d921ce63845d706e77209f7522804bf46
David Pierre <davidpierrealves21@gmail.com>
2019-11-25 10:11:57 -0300

    Remove ExampleApp and adds home page

--- backend/github_monitor/urls.py
+++ backend/github_monitor/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url  # noqa
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
+from rest_framework.documentation import include_docs_urls
+from monitor import views as core_views
 
 
 import django_js_reverse.views
@@ -14,6 +16,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^docs/', include_docs_urls(title='Todo API')),
 
-    url(r'^$', TemplateView.as_view(template_name='exampleapp/itworks.html'), name='home'),
+    url(r'^$', core_views.home, name='home'),
 ]
--- backend/monitor/views.py
+++ backend/monitor/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+@login_required
+def home(request):
+    context = {
+        # 'avatar': request.user.github_avatar_url,
+        'login': request.user.username,
+    }
+    return render(request, 'monitor/index.html', context)
