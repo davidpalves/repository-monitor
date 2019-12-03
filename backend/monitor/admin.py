fd73ad832bc3fb637d213c93c81a2b47b8e00f0c
David Pierre <davidpierrealves21@gmail.com>
2019-12-02 21:42:43 -0300

    Add models to admin page

--- backend/monitor/admin.py
+++ backend/monitor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Repository, Commit, Author
+
+
+admin.site.register(Repository)
+admin.site.register(Author)
+admin.site.register(Commit)
