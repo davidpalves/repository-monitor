886f4c3f3d0545eecd0242ee995bda874db4b990
David Pierre <davidpierrealves21@gmail.com>
2019-12-06 09:00:10 -0300

    Reorganize not found page text

--- frontend/js/pages/NotFoundPage.js
+++ frontend/js/pages/NotFoundPage.js
@@ -7,9 +7,9 @@ const NotFoundPage = () => {
   return (
     <div className="not-found-wrapper">
       <h1 className="not-found-title">If you are going, go in peace.</h1>
-      <h4 className="not-found-subtitle">But, if you are just lost</h4>
+      <h4 className="not-found-subtitle">But, if you are just lost, I suggest you to</h4>
       <Link className="redirect-home" to="/">
-        I suggest you to go Home
+        Go Home
       </Link>
     </div>
   );
