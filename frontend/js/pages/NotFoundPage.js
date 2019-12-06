b3cd439475158aaf4110e2c87213260604becc32
David Pierre <davidpierrealves21@gmail.com>
2019-12-06 08:05:18 -0300

    Fix scss selector conflict

--- frontend/js/pages/NotFoundPage.js
+++ frontend/js/pages/NotFoundPage.js
@@ -6,8 +6,8 @@ import '../../sass/pages/404.scss';
 const NotFoundPage = () => {
   return (
     <div className="not-found-wrapper">
-      <h1 className="title">If you are going, go in peace.</h1>
-      <h4 className="subtitle">But, if you are just lost</h4>
+      <h1 className="not-found-title">If you are going, go in peace.</h1>
+      <h4 className="not-found-subtitle">But, if you are just lost</h4>
       <Link className="redirect-home" to="/">
         I suggest you to go Home{' '}
       </Link>
