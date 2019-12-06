3bb279dc007c0cb8aa9564b5859aa59f97fe269b
David Pierre <davidpierrealves21@gmail.com>
2019-12-06 08:02:56 -0300

    Fix login page position placement

--- frontend/js/pages/NotFoundPage.js
+++ frontend/js/pages/NotFoundPage.js
@@ -5,7 +5,7 @@ import '../../sass/pages/404.scss';
 
 const NotFoundPage = () => {
   return (
-    <div className="wrapper">
+    <div className="not-found-wrapper">
       <h1 className="title">If you are going, go in peace.</h1>
       <h4 className="subtitle">But, if you are just lost</h4>
       <Link className="redirect-home" to="/">
